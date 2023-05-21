14ac733fcc6bbf21d09928ef7e9b6d23ade4bbc2
m_uellermichael <m_uellermichael@gmx.de>
2023-05-21 16:41:33 +0200

    working state

--- ar_game/opencv_pyglet.py
+++ ar_game/opencv_pyglet.py
@@ -212,7 +212,7 @@ def get_finger(frame):
     kernel = np.ones((5, 5), np.uint8)
     # Convert the frame to grayscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
+    ret, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
 
     # https://docs.opencv.org/4.x/d9/d61/tutorial_py_morphological_ops.html
     thresh = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
